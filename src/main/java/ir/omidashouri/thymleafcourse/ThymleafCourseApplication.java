9ccbabf6b28630e749fee32b897354291863758f
omidashouri <omidashouri@gmail.com>
2021-09-29 23:18:49 +0330

    Initial commit : 00.07.07 : omidashouri

---
+++ src/main/java/ir/omidashouri/thymleafcourse/ThymleafCourseApplication.java
@@ -0,0 +1,13 @@
+package ir.omidashouri.thymleafcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymleafCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/omidashouri/thymleafcourse/ThymleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.omidashouri.thymleafcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
