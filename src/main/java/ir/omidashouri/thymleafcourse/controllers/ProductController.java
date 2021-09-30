5ace705185288ef5a4fdf9bba8e6eac0b19726b0
omidashouri <omidashouri@gmail.com>
2021-09-30 13:48:50 +0330

    4. Rendering Thymeleaf Templates with Spring Boot and Spring MVC->finished : omidashouri : 00.07.08

---
+++ src/main/java/ir/omidashouri/thymleafcourse/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package ir.omidashouri.thymleafcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+
+    @RequestMapping("/product")
+    public String getProduct(){
+        return "product";
+    }
+}
