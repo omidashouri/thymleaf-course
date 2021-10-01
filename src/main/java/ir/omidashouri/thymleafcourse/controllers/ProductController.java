4bbb4d52b675b1899d47af4422d54832cf9b8a31
omidashouri <omidashouri@gmail.com>
2021-10-01 20:26:21 +0330

    7. Displaying Objects Using Thymeleaf with Spring MVC->finished : omidashouri : 00.07.09

--- src/main/java/ir/omidashouri/thymleafcourse/controllers/ProductController.java
+++ src/main/java/ir/omidashouri/thymleafcourse/controllers/ProductController.java
@@ -1,13 +1,38 @@
 package ir.omidashouri.thymleafcourse.controllers;
 
+import ir.omidashouri.thymleafcourse.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
 
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    /*@RequestMapping("/product")
+    public String getProduct(){
+        return "product";
+    }*/
+
     @RequestMapping("/product")
     public String getProduct(){
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProductById(@PathVariable Integer id, Model model){
+
+        model.addAttribute("product", productService.getProduct(id));
+
         return "product";
     }
 }
